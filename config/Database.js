9d8d0c5b20bd6a679f2776b8154bda556c2ba87d
kenaryark <kenarya5@gmail.com>
2024-11-20 14:34:00 +0700

    add database

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("consolerent", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  process.env.MYSQL_DB,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: "mysql",
+  }
+);
 
 export default db;
