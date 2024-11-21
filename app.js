3ca35549d08edfd2adfb1d4777d250a30a6f0736
kenaryark <kenarya5@gmail.com>
2024-11-21 14:45:11 +0700

    tes origin localhost midtrans

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(
     credentials: true,
     // origin: "*",
     // origin: "https://consolerent-test.netlify.app",
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
   })
 );
 app.use(cookieParser());
